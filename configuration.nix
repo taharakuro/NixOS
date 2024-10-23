1e5f4a8287325ea3f799e87650753646ead9dffa
Tahara <96952004+taharakuro@users.noreply.github.com>
2024-10-23 20:58:35 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,6 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.devices = [ "/dev/sda" ];  # Укажите здесь ваш диск
 
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
@@ -25,7 +23,7 @@
     description = "Tahara";
     shell = pkgs.bash;
     extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "1";  # Простой незашифрованный пароль
+    initialPassword = "1";
   };
 
   system.stateVersion = "24.05";
