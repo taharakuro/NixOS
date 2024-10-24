d02b72b447430bc8ce2d8da0e09c424d9161f7fa
Tahara <96952004+taharakuro@users.noreply.github.com>
2024-10-24 07:17:51 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,21 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.devices = [ "/dev/sda" ];
 
+  # Swap для подкачки
+  swapDevices = [ { device = "/dev/sda2"; } ];
+
+  # Btrfs root
+  fileSystems."/" = {
+    device = "/dev/sda3";
+    fsType = "btrfs";
+  };
+
+  # EFI boot
+  fileSystems."/boot" = {
+    device = "/dev/sda1";
+    fsType = "vfat";
+  };
+
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
