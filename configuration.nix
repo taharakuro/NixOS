081b3ae4d7680c31182f8c9302cf62deb71c7f36
Tahara <96952004+taharakuro@users.noreply.github.com>
2024-10-25 08:12:02 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,41 +1,48 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
+  imports = 
+    [ 
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
+  # Use the systemd-boot boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Install Hyprland
+  # Install Hyprland and other system packages
   environment.systemPackages = with pkgs; [
     kitty
     firefox
   ];
 
-  programs.hyprland.enable = true; # enable Hyprlan
-  # Включение службы для запуска Hyprland
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true; # LightDM
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # Enable Hyprland
+  programs.hyprland.enable = true;
 
+  # Enable X server and display manager (GDM)
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  # Enable NetworkManager for networking
+  networking.networkmanager.enable = true;
+
+  # Enable experimental features for Nix
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Set your time zone.
+  # Set your time zone
   time.timeZone = "Europe/Moscow";
 
-  # Define a user account. Don't forget to set a password with passwd. 
+  # Define a user account with initial password (change it later)
   users.users.tahara = {
     isNormalUser = true;
     home = "/home/tahara";
     description = "Tahara";
     shell = pkgs.bash;
     extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "1";
+    initialPassword = "1"; # Don't forget to change this password later!
   };
 
+  # Define the state version of NixOS
   system.stateVersion = "24.05";
 }
