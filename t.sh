4f3034d289f83c4be56703113f1916dd16455d76
taharakuro <vovak6819@gmail.com>
2026-07-18 00:37:04 +0300

    s

--- t.sh
+++
@@ -1,5 +0,0 @@
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko config/disko.nix
-nixos-generate-config --root /mnt
-cp config/* /mnt/etc/nixos/
-nix --extra-experimental-features "nix-command flakes" flake lock /mnt/etc/nixos
-nixos-install --flake /mnt/etc/nixos#nixos
