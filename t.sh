9e0e60af902bb767d9e885033afd799e10807123
taharakuro <vovak6819@gmail.com>
2026-07-17 21:25:49 +0300

    t

--- t.sh
+++ t.sh
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./disko.nix
-nixos-generate-config --root /mnt
-cp config/* /mnt/etc/nixos/
-nix --extra-experimental-features "nix-command flakes" flake lock /mnt/etc/nixos
-nixos-install --flake /mnt/etc/nixos#nixos
-=======
 nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko config/disko.nix
 nixos-generate-config --root /mnt
 cp config/* /mnt/etc/nixos/
 nix --extra-experimental-features "nix-command flakes" flake lock /mnt/etc/nixos
 nixos-install --flake /mnt/etc/nixos#nixos
->>>>>>> bc40f3f (	renamed:    disko.nix -> config/disko.nix)
