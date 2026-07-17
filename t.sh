39649db6029e3e3987785d064484434ecd7312e5
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-07-17 20:53:46 +0300

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,4 +1,5 @@
 nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko ./disko.nix
 nixos-generate-config --root /mnt
 cp config/* /mnt/etc/nixos/
+nix --extra-experimental-features "nix-command flakes" flake lock /mnt/etc/nixos
 nixos-install --flake /mnt/etc/nixos#nixos
