0d96919d66f7876b0be01432401a709fe14c04f1
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-07-18 20:44:19 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -11,13 +11,13 @@
 
   system.stateVersion = "26.05";
 
+  nixpkgs.config.allowUnfree = true;
+
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
     };
-
-    allowUnfree = true;
 
     gc = {
       automatic = true;
