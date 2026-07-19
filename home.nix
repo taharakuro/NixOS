d72d00678dc2a08c1b1d77c9bb031922125b19fb
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-07-19 18:01:33 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -56,7 +56,7 @@
     gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
     enable = true;
     iconTheme = {
-      name = "Papirus-Dark";
+      name = "Papirus-Light";
       package = pkgs.papirus-icon-theme;
     };
   };
