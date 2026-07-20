300214b99209a5ee7582df7ffcf7d9b1de914b8f
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-07-20 05:27:24 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,8 +7,14 @@
   # Этот блок читает сама команда `nix`, поэтому кэш работает и на установке —
   # но nix спросит подтверждение (или нужен флаг --accept-flake-config).
   nixConfig = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+    extra-substituters = [
+		"https://cache.nixos.org"
+		"https://noctalia.cachix.org"
+	];
+    extra-trusted-public-keys = [
+		"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+		"noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+	];
   };
 
   inputs = {
