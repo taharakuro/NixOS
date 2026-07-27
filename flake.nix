e1a6d75fdf9f4c3eff5a21509b63faab129748d7
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-07-27 14:21:45 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,12 +10,10 @@
     extra-substituters = [
       "https://cache.nixos.org"
       "https://noctalia.cachix.org"
-      "https://prismlauncher.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
     ];
   };
 
@@ -41,11 +39,9 @@
     # Официальный README PrismLauncher прямо предупреждает об этом.
     # Экономия на дублировании nixpkgs в лок-файле того не стоит.
     noctalia.url = "github:noctalia-dev/noctalia/cachix";
-
-    prismlauncher.url = "github:PrismLauncher/PrismLauncher";
   };
 
-  outputs = { nixpkgs, home-manager, disko, prismlauncher, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, disko, ... }@inputs: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -57,10 +53,6 @@
         disko.nixosModules.disko
         ./disko.nix
         ./configuration.nix
-        (
-          { pkgs, ... }:
-          { environment.systemPackages = [ prismlauncher.packages.${pkgs.system}.prismlauncher ]; }
-        )
         home-manager.nixosModules.home-manager
         {
           home-manager = {
