d23adc38a5a929642f3a41a1664610a882947379
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-08-30 00:33:38 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -45,10 +45,6 @@ in
     hostName = "nixos";
     networkmanager.enable = true;
     firewall.enable = true;
-    proxy = {
-      default = "http://127.0.0.1:8118";
-      noProxy = "127.0.0.1,localhost";
-    };
   };
 
   time.timeZone = "Europe/Moscow";
