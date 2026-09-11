528807b5ba56940d8d473453df0f49fd3fd366c2
Tahara <96952004+taharakuro@users.noreply.github.com>
2026-09-12 00:42:58 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -44,7 +44,6 @@
       inputs.hytale-launcher.packages.${pkgs.system}.default
       vintagestory
       inputs.prismlauncher.packages.${pkgs.system}.prismlauncher
-      amnezia-vpn
     ];
   };
   dconf = {
